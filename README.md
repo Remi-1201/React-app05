ba9b71085f9200ae299b54ee8cd1d9404fa2f134
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-06-16 17:39:34 +0900

    1- Inline Styles

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # [Udemy-React に入門した人のためのもっと React が楽しくなるステップアップコース完全版](https://www.udemy.com/course/react_stepup/learn/lecture/24823340#overview)
 
+1. Inline Styles
+
 ## 成果
 
 [Sample app 05 (React)](https://eie14m.csb.app/)
---
+++ src/components/inlineStyle.jsx
@@ -0,0 +1,27 @@
+export const InlineStyle = () => {
+  const containerStyle = {
+    border: "solid 2px #c5dedd",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "8px",
+    display: "flex",
+    alignItems: "center"
+  };
+  const titleStyle = {
+    margin: 0,
+    color: "#003049"
+  };
+  const buttonStyle = {
+    backgroundColor: "#fde2e4",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  };
+
+  return (
+    <div style={containerStyle}>
+      <p style={titleStyle}>- Inline Styles -</p>
+      <button style={buttonStyle}>FIGHT!!</button>
+    </div>
+  );
+};
